ab3b024d44e878fce72833e79f8854ed3fc45e63
fvargas88 <fvargas_88@hotmail.com>
2023-03-03 13:10:43 -0300

    LinkedHashSet velocidade intermediária e mantém a ordem dos elementos conforme eles forem inseridos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
 	public static void main(String[] args) {
-		Set<String> set = new TreeSet<>();
+		Set<String> set = new LinkedHashSet<>();
 		set.add("TV");
-		set.add("Notebook");
 		set.add("Tablet");
+		set.add("Notebook");
 		System.out.println(set.contains("Notebook"));
 		for (String p : set) {
 			System.out.println(p);
