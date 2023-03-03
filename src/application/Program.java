82ca66dc0a42584146d0b25c32a8fcc124c5ba44
fvargas88 <fvargas_88@hotmail.com>
2023-03-03 13:07:47 -0300

    Método TreeSet, o mais lento, porém ordena os dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 	public static void main(String[] args) {
-		Set<String> set = new HashSet<>();
+		Set<String> set = new TreeSet<>();
 		set.add("TV");
 		set.add("Notebook");
 		set.add("Tablet");
